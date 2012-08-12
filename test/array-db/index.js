5e73312833d9f951bcf1c90fe57ed6d00a14a14d
E.Azer Koçulu <azer@kodfabrik.com>
2012-08-11 23:35:51 -0700

    rm all tests

--- test/array-db/index.js
+++
@@ -1,7 +0,0 @@
-var map    = require('../../lib'),
-    driver = require('./driver'),
-    types  = map.types;
-
-module.exports = map.newDriver(driver);
-module.exports.types = types;
-module.exports.errors = map.errors;
--- test/content/capitalize.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(_, _, _, str){
-
-  return str ? str.replace(/(^\w)|(\s\w)/g, function(letter){
-    return letter.toUpperCase();
-  }) : undefined;
-
-};
